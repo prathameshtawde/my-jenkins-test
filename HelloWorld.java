e09a578515a84cbf4733c86cbdc9f17abb5afc83
prathameshtawde <36947042+prathameshtawde@users.noreply.github.com>
2018-05-01 21:56:22 -0500

    Add Hello World Java and Class file

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+/**This is a simple Hello World Java Program
+ * 
+ */
+
+/**
+ * @author Prathamesh
+ *
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
